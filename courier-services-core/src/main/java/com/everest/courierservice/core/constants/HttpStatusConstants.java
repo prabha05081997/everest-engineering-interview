6ac5f3a1e9077b25be9ca69856caf398e83e605f
manoj <manoj@digitap.ai>
2022-05-22 16:01:38 +0530

    question pdf added to project repo

---
+++ courier-services-core/src/main/java/com/everest/courierservice/core/constants/HttpStatusConstants.java
@@ -0,0 +1,8 @@
+package com.everest.courierservice.core.constants;
+
+public class HttpStatusConstants {
+
+    public static final int BAD_REQUEST_STATUS_CODE = 400;
+    public static final int UNKNOW_ERROR_STATUS_CODE = 500;
+
+}
--- courier-services-core/src/main/java/com/everest/courierservice/core/exception/ExceptionController.java
+++ courier-services-core/src/main/java/com/everest/courierservice/core/exception/ExceptionController.java
@@ -2,6 +2,8 @@ package com.everest.courierservice.core.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
+import static com.everest.courierservice.core.constants.HttpStatusConstants.*;
+
 @Slf4j
 public class ExceptionController {
 
@@ -12,13 +14,13 @@ public class ExceptionController {
     public static String getErrorResponse(Exception e) {
         if (e instanceof InputValidationException) {
             log.error("in input validation exception");
-            errorResponse = "400 - " + e.getMessage();
-        } else if (e instanceof NotFoundException) {
+            errorResponse = BAD_REQUEST_STATUS_CODE + " - " + e.getMessage();
+        } else if (e instanceof ServiceException) {
             log.error("in not found exceptiom with exception");
-            errorResponse = "404 - " + e.getMessage();
+            errorResponse = UNKNOW_ERROR_STATUS_CODE + " - " + e.getMessage();
         } else {
             log.error("in generic exceptiom with exception");
-            errorResponse = "500 - " + e.getMessage();
+            errorResponse = UNKNOW_ERROR_STATUS_CODE + " - " + e.getMessage();
         }
         return errorResponse;
     }
